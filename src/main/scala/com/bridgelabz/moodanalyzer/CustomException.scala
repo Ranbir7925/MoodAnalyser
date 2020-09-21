e3b9a0a1be3f7b6a127395b34c4065f9cdba5b13
Ranbir7925 <honeyranbir7925@gmail.com>
2020-09-21 21:21:24 +0530

    [RANBIR]ADDED:CustomException object to provide proper message

--- src/main/scala/com/bridgelabz/moodanalyzer/CustomException.scala
+++ src/main/scala/com/bridgelabz/moodanalyzer/CustomException.scala
@@ -2,6 +2,7 @@ package com.bridgelabz.moodanalyzer
 
 object CustomException extends Enumeration {
   type CustomException = Value
+
   val nullString =Value("Message can't be null")
   val emptyString =Value("Message length must be greater then zero")
 }
